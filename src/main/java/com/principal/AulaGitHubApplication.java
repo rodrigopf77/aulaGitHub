301885297d4fc30508f76546393ff8d7a8752f2b
Rodrigo Fernandes <rodrigofernandes3@hotmail.com>
2023-01-23 16:00:53 -0300

    Teste de commit

---
+++ src/main/java/com/principal/AulaGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaGitHubApplication.class, args);
+	}
+
+}
